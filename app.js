b1c40cdc48650d14faadc648f3e5ae691cc61d9f
Iacami Gevaerd <enapupe@gmail.com>
2023-11-05 20:05:45 -0300

    stream file directly from bucket; add /version endpoint
    this was an attempt to reduce latency by streaming the files directly from google bucket;
    no performance improvement could be noticed.
    
    also add a /version endpoint that outputs just the commit hash of the running server

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const express = require("express");
 const sharp = require("sharp");
+const { Storage, ApiError } = require("@google-cloud/storage");
 
+const storage = new Storage();
 const app = express();
+
 const PORT = 8080;
 const HOST = "0.0.0.0";
 const BASE_STORAGE_IMAGE_URL = "https://storage.googleapis.com/";
 const BUCKET = process.env.STORAGE_BUCKET || "openbeta-prod";
 
-const getImage = (path) =>
-  fetch(path).then(async (r) => ({
-    data: await r.arrayBuffer(),
-    status: r.status,
-  }));
 const getFormat = (webp, avif) => {
   return webp ? "webp" : avif ? "avif" : "jpeg";
 };
@@ -22,36 +18,44 @@ app.get("/healthy", (req, res) => {
   res.send("yep.");
 });
 
+app.get("/version", (req, res) => {
+  res.send(process.env.APP_VERSION);
+});
+
 app.get("*", async (req, res) => {
   try {
-    const { searchParams, pathname, href } = new URL(
-      `${BASE_STORAGE_IMAGE_URL}${BUCKET}${req.url}`,
-    );
-
-    if (!/\.(jpe?g|png|gif|webp)$/i.test(pathname)) {
+    if (!/\.(jpe?g|png|gif|webp)$/i.test(req.path)) {
       return res.status(400).send("Disallowed file extension");
     }
 
     const webp = req.headers.accept?.includes("image/webp");
     const avif = req.headers.accept?.includes("image/avif");
-    const quality = Number(searchParams.get("q")) || 90;
-    const width = Number(searchParams.get("w")) || undefined;
-    const height = Number(searchParams.get("h")) || undefined;
+    const quality = Number(req.query.q) || 90;
+    const width = Number(req.query.w) || undefined;
+    const height = Number(req.query.h) || undefined;
     const format = getFormat(webp, avif);
-
-    const { data, status } = await getImage(href);
-    if (status > 399) {
-      return res
-        .status(415)
-        .send("upstream server did not respond with a valid status code");
-    }
 
     res
       .set("Cache-Control", "public, max-age=15552000")
       .set("Vary", "Accept")
       .type(`image/${format}`);
 
-    sharp(data)
+    const pipeline = sharp();
+
+    storage
+      .bucket(BUCKET)
+      .file(req.path.slice(1)) // remove leading slash
+      .createReadStream()
+      .on("error", function (e) {
+        if (e instanceof ApiError) {
+          if (e.message?.includes("No such object"))
+            return res.status(404).end();
+        }
+        return res.status(500).send(JSON.stringify(e));
+      })
+      .pipe(pipeline);
+
+    pipeline
       .rotate()
       .resize({ width, height })
       .toFormat(format, { effort: 3, quality, progressive: true })
